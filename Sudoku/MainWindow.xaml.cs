70751e75555b03c6a16559e970dce35458fba8b4
Martin <hazi.martin@gmail.com>
2021-03-14 16:25:06 +0100

    Created basic layout

--- Sudoku/MainWindow.xaml.cs
+++ Sudoku/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace Sudoku
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int SIZE = 9;
+
         public MainWindow()
         {
             InitializeComponent();
+            initGird();
+        }
+
+        // Adds row and column definitions to equally align elements based on the size of the grid
+        private void initGird()
+        {
+            RowDefinition rowDef;
+            ColumnDefinition colDef;
+            GridLength gridLength = new GridLength(0, GridUnitType.Auto);
+
+            for (int idx = 0; idx < SIZE; idx++)
+            {
+                rowDef = new RowDefinition();
+                rowDef.Height = gridLength;
+                sudokuGrid.RowDefinitions.Add(rowDef);
+
+                colDef = new ColumnDefinition();
+                colDef.Width = gridLength;
+                sudokuGrid.ColumnDefinitions.Add(colDef);
+            }
         }
     }
 }
