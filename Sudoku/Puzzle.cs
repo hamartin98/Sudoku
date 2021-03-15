997b5f07ee22233babaf867587fde047e4a6ee4b
Martin <hazi.martin@gmail.com>
2021-03-15 11:28:50 +0100

    Added sum ckecking functions to the Puzzle class

--- Sudoku/Puzzle.cs
+++ Sudoku/Puzzle.cs
@@ -13,8 +13,14 @@ namespace Sudoku
         // find solution
         // check if a row, col or a box is safe
 
+        /*
+        A board is solved when every row, box and column contains distinct numbers
+        If the sum of the numbers in a row, a col or a box not equal 45, we don't need to check for distinct values in it
+        */
+
         private List<List<int>> data { get; }
-        private int size = 9;
+        private const int SIZE = 9;
+        private const int BOXSIZE = 3;
         public Puzzle(List<List<int>> data)
         {
             this.data = data;
@@ -30,6 +36,68 @@ namespace Sudoku
         public void SetValue(int value, int row, int col)
         {
             data[row][col] = value;
+        }
+
+        // returns the starting row of the box containing the given row
+        private int StartRow(int row)
+        {
+            return row - (row % 3);
+        }
+
+        // returns the starting column of the box containing the given column
+        private int StartCol(int col)
+        {
+            return col - (col % 3);
+        }
+
+        // check the sum of the given row
+        private bool IsRowSumOk(int row)
+        {
+            int sum = 0;
+
+            for(int col = 0; col < SIZE; col++)
+            {
+                sum += data[row][col];
+            }
+
+            return sum == 45;
+        }
+
+        // check the sum of the given column
+        private bool IsColSumOk(int col)
+        {
+            int sum = 0;
+
+            for(int row = 0; row < SIZE; row++)
+            {
+                sum += data[row][col];
+            }
+
+            return sum == 45;
+        }
+
+        // check the sum of the given box
+        private bool IsBoxSumOk(int row, int col)
+        {
+            int startRow = StartRow(row);
+            int startCol = StartCol(col);
+            int sum = 0;
+
+            for(int rowIdx = 0; rowIdx < BOXSIZE; rowIdx++)
+            {
+                for(int colIdx = 0; colIdx < BOXSIZE; colIdx++)
+                {
+                    sum += data[startRow + rowIdx][startCol + colIdx];
+                }
+            }
+
+            return sum == 45;
+        }
+
+        // checks row, column and box sums from the given row and column
+        private bool CheckSums(int row, int col)
+        {
+            return IsRowSumOk(row) && IsColSumOk(col) && IsBoxSumOk(row, col);
         }
     }
 }
